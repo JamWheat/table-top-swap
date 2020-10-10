bc311947b977dd1b9b74d86b40ab98d4991a68ad
Jam Wheat <jamonwheat@gmail.com>
2020-10-10 11:06:32 -0400

    hide fields in add offer form, render result aspects on page

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Offer
+from django import forms
 
 class OfferForm(ModelForm):
   class Meta:
@@ -7,3 +8,11 @@ class OfferForm(ModelForm):
     # fields = ['title', 'bgg_slug', 'year_published', 'image', 'thumbnail', 'condition', 'comment']
     # exclude = ['title', 'bgg_slug', 'year_published', 'image', 'thumbnail', 'user']
     fields = '__all__'
+    widgets = {
+      'user': forms.HiddenInput(),
+      'title': forms.HiddenInput(),
+      'bgg_slug': forms.HiddenInput(),
+      'image': forms.HiddenInput(),
+      'thumbnail': forms.HiddenInput(),
+      'year_published': forms.HiddenInput(),
+    }
